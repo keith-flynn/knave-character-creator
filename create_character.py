20a05e858c1b8d7f0181307b8614cb589111c977
Keith Flynn <flynn.keith@gmail.com>
2022-11-06 20:42:24 -0500

    working condition

--- create_character.py
+++ create_character.py
@@ -1,4 +1,24 @@
 import pandas as pd
+import random
 
 traits = pd.read_csv('assets/knave-traits.csv')
 print(traits)
+
+traits_list = ['PHYSIQUE', 'FACE', 'SKIN', 'HAIR', 
+               'CLOTHING', 'VIRTUE', 'VICE', 'SPEECH', 
+               'BACKGROUND', 'MISFORTUNES', 'ALIGNMENT'
+               ]
+
+roll_result = []
+
+def roll():
+    # There are 11 attribute columns
+    for i in range(11):
+        roll = random.randint(1, 20)
+        roll_result.append(roll)
+
+roll()
+print(roll_result)
+
+for (trait, ele) in zip(traits_list, roll_result):
+    print(trait, ':', traits.at[(ele - 1), trait])
