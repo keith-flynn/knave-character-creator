1c358aada6da6ae4267c5d53316b4bee34fad18a
Keith Flynn <flynn.keith@gmail.com>
2022-11-13 10:42:11 -0500

    iterate through column names instead of referencing list, much cleaner

--- create_character.py
+++ create_character.py
@@ -4,23 +4,19 @@ import random
 # Dice roller
 def roll():
     # There are 11 attribute columns
-    for i in range(11):
+    for die in range(11):
         roll = random.randint(1, 20)
         roll_result.append(roll)
 
+# Import traits and set the index to dice rolls
 traits = pd.read_csv('assets/knave-traits.csv')
 traits = traits.set_index('ROLL')
 print(traits)
-
-traits_list = ['PHYSIQUE', 'FACE', 'SKIN', 'HAIR', 
-               'CLOTHING', 'VIRTUE', 'VICE', 'SPEECH', 
-               'BACKGROUND', 'MISFORTUNES', 'ALIGNMENT'
-               ]
 
 roll_result = []
 
 roll()
 print(roll_result)
 
-for (trait, ele) in zip(traits_list, roll_result):
+for (trait, ele) in zip(list(traits), roll_result):
     print(trait, ':', traits.at[ele, trait])
